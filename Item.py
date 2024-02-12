f425fa3912a4905f6454803bf6c35621f64ef759
Aarshee Bhattacharya <48876044+AarsheeB@users.noreply.github.com>
2024-02-11 20:48:42 -0600

    Update Item.py

--- Item.py
+++ Item.py
@@ -1,12 +1,14 @@
 from django.db import models
 
 class Item(models.Model):
+    sku = models.CharField(max_length=50, unique=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
-    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    category = models.ForeignKey('ProductCategory', on_delete=models.CASCADE)
+    supplier = models.ForeignKey('Supplier', on_delete=models.CASCADE)
+    tags = models.ManyToManyField('Tag')
     # Add other relevant fields
 
     def __str__(self):
