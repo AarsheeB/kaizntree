6baf12f22fe523f4fac786fc38149799da519f8e
Aarshee Bhattacharya <48876044+AarsheeB@users.noreply.github.com>
2024-02-11 21:19:27 -0600

    Update Itemview.py

--- Itemview.py
+++ Itemview.py
@@ -1,11 +1,36 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Item
 from .serializers import ItemSerializer
 
 class ItemListCreateView(generics.ListCreateAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category', 'tags', 'quantity', 'price', 'supplier', 'stock_status']
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Item.objects.all()
+
+        # Example: Filtering based on date range (modify as needed)
+        start_date = self.request.query_params.get('start_date', None)
+        end_date = self.request.query_params.get('end_date', None)
+        if start_date and end_date:
+            queryset = queryset.filter(transaction_date__range=(start_date, end_date))
+
+        return queryset
 
 class ItemRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+
+
+
+
+
+
+
+
+
+
